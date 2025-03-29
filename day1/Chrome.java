1e6087705f2a33a29e1bf97e3e16edf25e079cd4
navani02 <navani02@gmail.com>
2025-03-29 19:56:09 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		System.out.println("Version: " + version);
+		
+		String developer = "Google";
+		System.out.println("Developer: " + developer);
+		
+		boolean isBeta = false;
+		System.out.println("Is Beta: " + isBeta);
+		
+		int releaseYear = 2008;
+		System.out.println("Release Year: " + releaseYear);
+		
+		char shortcutKey = 'C';
+		System.out.println("Shortcut Key: " + shortcutKey );
+
+	}
+
+}
